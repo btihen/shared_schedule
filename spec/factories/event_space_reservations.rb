bbd779e11eda6360785377316910b77b23bf7c06
Bill Tihen <btihen@gmail.com>
2020-02-23 19:34:37 +0100

    quick file tidy

--- spec/factories/event_space_reservations.rb
+++ spec/factories/event_space_reservations.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :event_space_reservation do
 
-    # temp value incase a tenant is not passed in
     transient do
       tenant  { FactoryBot.create :tenant }
     end
