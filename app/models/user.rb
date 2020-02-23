ef99a4a8b8d8dd358c25f3f3108ee2eb1a9966d5
Bill Tihen <btihen@gmail.com>
2020-02-23 20:01:27 +0100

    remove silly blank

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
           :recoverable, :rememberable, :validatable
 
   belongs_to :tenant
-  
+
   has_many :user_interests, inverse_of: :user, dependent: :destroy
   has_many :interests, through: :user_interests, source: :reason
 
