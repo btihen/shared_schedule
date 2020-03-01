3a7dd6c1062f180a73d56808884477635c0c7d5d
Bill Tihen <btihen@gmail.com>
2020-03-01 12:46:58 +0100

    factory improvement for logos

--- spec/factories/tenants.rb
+++ spec/factories/tenants.rb
@@ -3,7 +3,8 @@ FactoryBot.define do
     sequence(:tenant_name)  { |n| "#{Faker::Company.name} #{n}" }
     tenant_tagline          { Faker::Company.catch_phrase }
     tenant_site_url         { Faker::Internet.url }
-    tenant_logo_url         { Faker::LoremFlickr.image(size: "96x96", search_terms: ['room']) }  # { Faker::Company.logo }
+    # https://loremflickr.com/96/96/sports
+    tenant_logo_url         { Faker::LoremFlickr.image(size: "96x96", search_terms: ["#{Faker::Team.sport}"]) }
     tenant_description      { Faker::Lorem.paragraph(sentence_count: rand(1..3)) }
   end
 end
