0da35951e3427c18ec79f298eb7b7b4e0232e24b
Bill Tihen <btihen@gmail.com>
2020-03-03 17:03:31 +0100

    integrate JS the rails way

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("bulma-extensions")
+// require("bulma/calendar")
+require("bulma/modal")
+require("bulma/navbar_hamburger")
+// require("bulma-extensions")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
