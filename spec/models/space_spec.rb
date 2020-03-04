ae92d728f26e5c59811ccddf6206aa98f07130c6
Bill Tihen <btihen@gmail.com>
2020-03-04 18:07:40 +0100

    test tidy

--- spec/models/space_spec.rb
+++ spec/models/space_spec.rb
@@ -82,21 +82,11 @@ RSpec.describe Space, type: :model do
   describe "DB settings" do
     it { have_db_index(:space_name) }
     it { is_expected.to have_db_column(:space_location) }
-    it { is_expected.to have_db_column(:is_calendar_public) }
+    # it { is_expected.to have_db_column(:is_calendar_public) }
     it { is_expected.to have_db_column(:is_double_booking_ok) }
   end
 
   context "model methods" do
-    describe "#is_calendar_public?" do
-      it "returns false by default" do
-        model = FactoryBot.build :space
-        expect(model.is_calendar_public?).to be_falsey
-      end
-      it "returns true when explicity set to true" do
-        model = FactoryBot.build :space, is_calendar_public: true
-        expect(model.is_calendar_public?).to be_truthy
-      end
-    end
     describe "#is_double_booking_ok?" do
       it "returns false by default" do
         model = FactoryBot.build :space
