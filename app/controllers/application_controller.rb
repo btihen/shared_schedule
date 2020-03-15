2378332d76e13b9f1c4ebdcfa073aa18cccece3a
Bill Tihen <btihen@gmail.com>
2020-03-15 18:02:29 +0100

    add redirects after sign-in and sign-out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    tenant_path(resource.tenant)
+  end
+
+  def after_sign_out_path_for(scope)
+    root_path
+  end
+
 end
